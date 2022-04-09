1187ec621171ddf38d09319b7187e11ad2242495
ilya kozubovskiy <ilyakozubovskiy@outlook.com>
2022-04-09 21:40:29 +0300

    Add project files.

---
+++ WebCamConsumer/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System.Configuration;
+using System.Drawing;
+using System.IO;
+using System.Net.Sockets;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+
+
+namespace WebCamConsumer
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+      
+        private async void Image_Loaded(object sender, RoutedEventArgs e)
+        {
+            int port;
+            int.TryParse(ConfigurationManager.AppSettings.Get("port"),out port);
+
+            using (UdpClient client = new UdpClient(port))
+            {  
+                double size = 0;
+                while (size!= double.MaxValue)
+                {
+                    var data = await client.ReceiveAsync();
+                    using (MemoryStream ms = new MemoryStream(data.Buffer))
+                    {
+                        BitmapImage bitmapimage = new BitmapImage();
+                        bitmapimage.BeginInit();
+                        bitmapimage.StreamSource = ms;
+                        bitmapimage.CacheOption = BitmapCacheOption.OnLoad;
+                        bitmapimage.EndInit();
+                        Image.Source = bitmapimage;
+                    }
+                   
+                    size += data.Buffer.Length / 1048576d;
+                    Title = $"Bytes recieved: {size.ToString("0.0")}";
+                }
+            }
+        } 
+    }
+}
---
+++ WebCamProducer/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Net;
+using System.Configuration;
+using AForge.Video.DirectShow;
+using System.Net.Sockets;
+using System.Drawing;
+using System.IO;
+using System.Drawing.Imaging;
+
+namespace WebCamProducer
+{
+   static class Program
+    {
+        private static IPEndPoint consumerEndPoint;
+        private static readonly UdpClient udpClient = new UdpClient();
+
+        static void Main()
+        {
+
+            int consumerPort = int.Parse(ConfigurationManager.AppSettings.Get("consumerPort"));
+            var consumerIp = ConfigurationManager.AppSettings.Get("consumerIp");
+            consumerEndPoint = new IPEndPoint(IPAddress.Parse(consumerIp), consumerPort);
+            Console.WriteLine($"Sending packets to: {consumerEndPoint}");
+            FilterInfoCollection videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            VideoCaptureDevice videoSource = new VideoCaptureDevice(videoDevices[0].MonikerString);
+            videoSource.NewFrame += VideoSource_NewFrame;
+            videoSource.Start();
+        }
+
+        private static void VideoSource_NewFrame(object sender, AForge.Video.NewFrameEventArgs eventArgs)
+        {
+            var bmp = new Bitmap(eventArgs.Frame, 1024, 768);
+            try
+            {
+                using (var ms = new MemoryStream())
+                {
+                    bmp.Save(ms, ImageFormat.Jpeg);
+                    var bytes = ms.ToArray();
+                    udpClient.Send(bytes, bytes.Length, consumerEndPoint);
+                }
+
+            }
+            catch (Exception)
+            {
+                //Continue sending frames
+            }
+        }
+    }
+
+}
